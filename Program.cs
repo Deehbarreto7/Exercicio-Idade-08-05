b12afa900d393d241e9cda49f40741416fa82e08
Débora <deehbarreto7@gmail.com>
2026-05-08 20:32:55 -0300

    idade

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Digite a sua idade:");
+
+int idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade >= 18)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Você é maior de idade.");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Você é menor de idade.");
+}
+
+Console.ResetColor();
+Console.WriteLine("Obrigado!");
